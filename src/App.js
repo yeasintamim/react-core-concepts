c8fe139ecf548cdbc77f26d30413e8cfc79acaac
yeasintamim <yeasin47@student.sust.edu>
2021-04-19 02:49:06 +0600

    second commit-JSX, Dynamic content, Dynamic Style in React-module32.4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
+import React from 'react'; 
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  var person = {
+    name : "yeasin motu",
+    job : 'Singer'
+  }
+
+  var person1 = {
+    name : " moti",
+    job : 'Dancer'
+  }
+  var style = {
+    color:'red',
+    backgroundColor:'yellow'
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit done <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <h1 style = {style}>My heading:{person.name +" "+person.job}</h1>
+        <h2 style = { {backgroundColor:'cyan',color:'yellow'} } > heading1 :{person1.name +" "+person1.job} </h2>
+        <p >My first react paragraph</p>
+        
       </header>
     </div>
   );
+}
+
+function Person(){
+  return <h1>Name:Sakib</h1>
 }
 
 export default App;
