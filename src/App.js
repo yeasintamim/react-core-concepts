2f759ebe907024a968ec76f80168e2e61567a48f
yeasintamim <yeasin47@student.sust.edu>
2021-04-20 01:41:54 +0600

    third commit::components character .......1. similar in look

--- src/App.js
+++ src/App.js
@@ -1,42 +1,31 @@
-import React from 'react'; 
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-
-  var person = {
-    name : "yeasin motu",
-    job : 'Singer'
-  }
-
-  var person1 = {
-    name : " moti",
-    job : 'Dancer'
-  }
-  var style = {
-    color:'red',
-    backgroundColor:'yellow'
-  }
-  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit done <code>src/App.js</code> and save to reload.
-        </p>
-
-        <h1 style = {style}>My heading:{person.name +" "+person.job}</h1>
-        <h2 style = { {backgroundColor:'cyan',color:'yellow'} } > heading1 :{person1.name +" "+person1.job} </h2>
-        <p >My first react paragraph</p>
-        
+       <p>i am a React Person</p>
+       <Person></Person>
+       <Person></Person>
+       <Person></Person>
       </header>
     </div>
   );
 }
 
 function Person(){
-  return <h1>Name:Sakib</h1>
+
+  const personStyle={
+
+     border:'2px solid red',
+     margin:'10px'
+  }
+  return <div style={personStyle}>
+  <h1>Name: Sakib</h1>
+  <h3>Hero of the Year</h3>
+  </div>
 }
 
 export default App;
