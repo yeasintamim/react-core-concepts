37dfccbdc3dbc52c3c03edd61ea17f0659d76fad
yeasintamim <yeasin47@student.sust.edu>
2021-04-20 20:21:34 +0600

    Pass dynamic data to components, props in react... 32.7module..example 3

--- src/App.js
+++ src/App.js
@@ -3,32 +3,32 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const nayoks = ['Anwar','Jafor','Alomgir','Salman']
+  
   return (
     <div className="App">
       <header className="App-header">
        <p>i am a React Person</p>
-       <Person nayok= {nayoks[0]} nayika ="Moushumi"></Person>
-       <Person nayok = {nayoks[1]} nayika ="Shabana"></Person>
-       <Person nayok = {nayoks[2]} nayika ="x1"></Person>
-       <Person nayok = {nayoks[3]} nayika ="x2"></Person>
-       
+       <Person name="Munna" job ="football"></Person>
+       <Person name="Masum" job ="dorsok"></Person>
       </header>
     </div>
   );
 }
 
 function Person(props){
+  return (
+    <div style ={{border:'2px solid gold',width:'400px'}}>
+        <h1>My Name: {props.name}</h1>
+        <h3>My Profession: {props.job}</h3>
+    </div>
+  )
 
-  const personStyle={
-
-     border:'2px solid red',
-     margin:'10px'
-  }
-  return <div style={personStyle}>
-  <h1>Name  : {props.nayok}</h1>
-  <h3>Hero of {props.nayika}</h3>
-  </div>
 }
 
+
 export default App;
+
+
+
+
+
